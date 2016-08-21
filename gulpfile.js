640521d7fad4700b3a3b9403cd68bb8b9670c90f
Stefan <spiazza@opera.com>
2016-08-21 15:20:15 +0100

    UPDATE: Basic boilerplate setup

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp')
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create();
+
+gulp.task('styles', function() {
+	gulp.src('./src/scss/styles.scss')
+		.pipe(sass({outputStyle: 'compressed'})
+		.pipe(gulp.dest('./dest/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+
+	gulp.watch('./src/scss/*.scss', ['styles']);
+	gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'serve']);

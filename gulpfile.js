6e11af6d683cba5f6ffdae3161169f383358e77f
Stefan <spiazza@opera.com>
2016-10-23 14:21:54 +0100

    UPDATE: Autoprefixer and error handling added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp')
 	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
 	browserSync = require('browser-sync').create();
+
 
 gulp.task('styles', function() {
 	gulp.src('./src/scss/styles.scss')
-		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({
+            browsers: ['> 1%', 'IE 7'],
+            cascade: false
+        }))
 		.pipe(gulp.dest('./dest/css'))
 		.pipe(browserSync.reload({stream: true}))
 });
